d7b265c9fdc95da29b930e65e1d101c325429b60
Alois Schloegl <alois.schloegl@tugraz.at>
2008-05-26 09:47:57 +0000

    EventSampleRate is not integer but float

--- src/base/signal_buffer.cpp
+++ src/base/signal_buffer.cpp
@@ -1,5 +1,29 @@
-// signal_buffer.cpp
+/*
 
+    $Id: signal_buffer.cpp,v 1.6 2008-05-26 09:47:57 schloegl Exp $
+    Copyright (C) Thomas Brunner  2006,2007 
+    		  Christoph Eibel 2007,2008, 
+		  Clemens Brunner 2006,2007,2008  
+    		  Alois Schloegl  2008
+    This file is part of the "SigViewer" repository 
+    at http://biosig.sf.net/ 
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 3
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+    
+*/
+
+// signal_buffer.cpp
 
 
 #include "file_signal_reader.h"
@@ -732,7 +756,7 @@ void SignalBuffer::removeEvent(uint32 event_id)
 
 // get event samplerate
 
-uint32 SignalBuffer::getEventSamplerate() const
+double SignalBuffer::getEventSamplerate() const
 
 {
 
