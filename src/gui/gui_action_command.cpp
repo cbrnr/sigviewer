78cdb66ff60bce896ecf05072ce631ba5cff3cdf
Christoph Eibel <christoph.eibel@tugraz.at>
2010-04-23 14:54:32 +0000

    sigviewer: replaced Q_ASSERT by cassert

--- src/gui/gui_action_command.cpp
+++ src/gui/gui_action_command.cpp
@@ -2,6 +2,8 @@
 #include "gui_action_factory.h"
 #include "../application_context.h"
 
+#include <cassert>
+
 namespace BioSig_
 {
 
@@ -14,12 +16,12 @@ GuiActionCommand::GuiActionCommand (QStringList const& action_ids)
     {
         action_map_[*iter] = new QAction (*iter, this);
         connectors_.push_back (new ActionConnector (this, *iter));
-        Q_ASSERT(connectors_.last ()->connect (action_map_[*iter], SIGNAL(triggered()), SLOT(trigger())));
-        Q_ASSERT(connect (connectors_.last (), SIGNAL(triggered(QString const&)), SLOT(trigger(QString const&))));
-        Q_ASSERT(action_map_[*iter]->connect (this, SIGNAL(qActionEnabledChanged(bool)), SLOT(setEnabled (bool))));
-        Q_ASSERT(connect (ApplicationContext::getInstance().data(), SIGNAL(stateChanged(ApplicationState)),
+        assert (connectors_.last ()->connect (action_map_[*iter], SIGNAL(triggered()), SLOT(trigger())));
+        assert (connect (connectors_.last (), SIGNAL(triggered(QString const&)), SLOT(trigger(QString const&))));
+        assert (action_map_[*iter]->connect (this, SIGNAL(qActionEnabledChanged(bool)), SLOT(setEnabled (bool))));
+        assert (connect (ApplicationContext::getInstance().data(), SIGNAL(stateChanged(ApplicationState)),
                           SLOT(applicationStateChanged(ApplicationState))));
-        Q_ASSERT(connect (ApplicationContext::getInstance().data(), SIGNAL(currentTabSelectionStateChanged(TabSelectionState)),
+        assert (connect (ApplicationContext::getInstance().data(), SIGNAL(currentTabSelectionStateChanged(TabSelectionState)),
                           SLOT(tabSelectionStateChanged (TabSelectionState))));
     }
 }
