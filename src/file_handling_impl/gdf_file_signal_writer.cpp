3684225d92b70318896a8f9d8999cf227cc4a2dc
Christoph Eibel <christoph.eibel@tugraz.at>
2010-07-21 05:57:03 +0000

    workaround: GDFFileSignalReader supports saving events to existing signal files by using libbiosig

--- src/file_handling_impl/gdf_file_signal_writer.cpp
+++ src/file_handling_impl/gdf_file_signal_writer.cpp
@@ -4,6 +4,8 @@
 #include "gdfwriter.h"
 #include "gui/progress_bar.h"
 
+#include "biosig.h"
+
 #include <QDebug>
 
 namespace BioSig_
@@ -32,12 +34,48 @@ QSharedPointer<FileSignalWriter> GDFFileSignalWriter::createInstance (QString co
 
 
 //-------------------------------------------------------------------------
-QString GDFFileSignalWriter::saveEventsToSignalFile (QSharedPointer<EventManager const>,
-                                        std::set<EventType> const&)
+QString GDFFileSignalWriter::saveEventsToSignalFile (QSharedPointer<EventManager const> event_manager,
+                                        std::set<EventType> const& types)
 {
+    // FIXXXME: REMOVE AS SOON AS LIBGDF SUPPORTS SAVING EVENTS TO EXISTING FILES
+    QList<EventID> events;
+    foreach (EventType type, types)
+        events.append(event_manager->getEvents(type));
 
+    unsigned number_events = events.size();
+    qDebug () << "number_events = " <<number_events;
+    qDebug () << "event_manager->getNumberOfEvents() = " << event_manager->getNumberOfEvents();
+    HDRTYPE* header = constructHDR (0, number_events);
 
-    return "NOT IMPLEMENTED YET!!!";
+    qDebug () << "GDFFileSignalWriter::saveEventsToSignalFile to " << new_file_path_;
+    header = sopen (new_file_path_.toStdString().c_str(), "r", header);
+    header->EVENT.SampleRate = event_manager->getSampleRate();
+    header->EVENT.N = number_events;
+    header->EVENT.TYP = (typeof(header->EVENT.TYP)) realloc(header->EVENT.TYP,number_events * sizeof(typeof(*header->EVENT.TYP)));
+    header->EVENT.POS = (typeof(header->EVENT.POS)) realloc(header->EVENT.POS,number_events * sizeof(typeof(*header->EVENT.POS)));
+    header->EVENT.CHN = (typeof(header->EVENT.CHN)) realloc(header->EVENT.CHN,number_events * sizeof(typeof(*header->EVENT.CHN)));
+    header->EVENT.DUR = (typeof(header->EVENT.DUR)) realloc(header->EVENT.DUR,number_events * sizeof(typeof(*header->EVENT.DUR)));
+
+    for (unsigned index = 0; index < number_events; index++)
+    {
+        QSharedPointer<SignalEvent const> event = event_manager->getEvent(events[index]);
+        if (event->getChannel() == UNDEFINED_CHANNEL)
+            header->EVENT.CHN[index] = 0;
+        else
+            header->EVENT.CHN[index] = event->getChannel() + 1;
+        header->EVENT.DUR[index] = event->getDuration();
+        header->EVENT.TYP[index] = event->getType ();
+        header->EVENT.POS[index] = event->getPosition();
+    }
+
+    int error = sflush_gdf_event_table (header);
+
+    sclose (header);
+    destructHDR (header);
+    if (error)
+        return "Events not saved!!!! (biosig error)";
+    else
+        return "";
 }
 
 //-------------------------------------------------------------------------
