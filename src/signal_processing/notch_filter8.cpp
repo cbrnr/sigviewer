5f7d3546f17760e72c4aa37c5b185b046794da12
Christoph Eibel <christoph.eibel@tugraz.at>
2010-09-28 09:34:09 +0000

    added missed files

---
+++ src/signal_processing/notch_filter8.cpp
@@ -0,0 +1,111 @@
+#include "notch_filter8.h"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <iostream>
+#include <iomanip>
+
+#include <complex>
+#include <stdexcept>
+
+namespace SigViewer_
+{
+
+
+//---------------------------------------------------------------------------------------
+
+using std::vector;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+const unsigned int NotchFilter8::FILTER_ORDER = 8;
+const double NotchFilter8::POLE_DISTANCE = 0.995;
+
+//---------------------------------------------------------------------------------------
+
+NotchFilter8::NotchFilter8(double fs, double freq)
+  :a_(FILTER_ORDER +1,0), b_(FILTER_ORDER+1,0), d_(POLE_DISTANCE),
+   fs_(fs), freq_(freq)
+{
+    cout << "fs = " << fs << endl;
+    cout << "freq = " << freq << endl;
+  if(freq >= fs/2)
+    throw(std::invalid_argument("Filter freq must be lower than fs/2!"));
+
+  if(!freq)
+    throw(std::invalid_argument("Filter freq must not be 0!"));
+
+  if(!fs)
+    throw(std::invalid_argument("Fs must not be 0!"));
+
+  double w = (2 * M_PI * freq)/fs;
+
+  std::complex<double> e1(0,w);
+  std::complex<double> e2(0,-w);
+
+  std::complex<double> v_comp = std::exp(e1) + std::exp(e2);
+
+  if(v_comp.imag())
+    throw(std::runtime_error("Coefficient factor has imaginary part -- something went wrong!"));
+
+  double v( v_comp.real() );
+
+  cout << "V: "<<  v << endl;
+
+  b_[0] = 1;
+  b_[1] = -4* v;
+  b_[2] = 4 + 6 * pow(v,2);
+  b_[3] = -12 * v -4 * pow(v,3);
+  b_[4] = 6 + 12 * pow(v,2) + pow(v,4);
+  b_[5] = b_[3];
+  b_[6] = b_[2];
+  b_[7] = b_[1];
+  b_[8] = b_[0];
+
+  a_[0] = b_[0];
+  a_[1] = b_[1] * d_;
+  a_[2] = b_[2] * pow(d_, 2);
+  a_[3] = b_[3] * pow(d_, 3);
+  a_[4] = b_[4] * pow(d_, 4);
+  a_[5] = b_[5] * pow(d_, 5);
+  a_[6] = b_[6] * pow(d_, 6);
+  a_[7] = b_[7] * pow(d_, 7);
+  a_[8] = b_[8] * pow(d_, 8);
+
+    cout.setf(std::ios::showpos);
+    for(unsigned int n = 0; n < a_.size(); n++)
+    {
+      cout << n <<" -- a: " << std::setw(14) << a_[n];
+      cout << std::setw(14) << ";  b: " << b_[n] << endl;
+    }
+
+}
+
+//---------------------------------------------------------------------------------------
+//template<typename T>
+//void NotchFilter8::filter (T& in, T& out, int in_out_size)
+//{
+
+//}
+
+//---------------------------------------------------------------------------------------
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/signal_processing/notch_filter8.h
@@ -0,0 +1,71 @@
+#ifndef NOTCH_FILTER8_H
+#define NOTCH_FILTER8_H
+
+//---------------------------------------------------------------------------------------
+
+#include "base/filter.h"
+
+#include <vector>
+#include <boost/circular_buffer.hpp>
+#include <iostream>
+
+namespace SigViewer_
+{
+
+//---------------------------------------------------------------------------------------
+class NotchFilter8 : public Filter
+{
+  public:
+    NotchFilter8(double fs, double freq);
+
+    virtual void filter (double* in, double* out, int in_out_length) const
+    {
+        filterImpl (in, out, in_out_length);
+    }
+
+private:
+
+
+            template<typename T>
+            void filterImpl (T const& in, T& out, int in_out_size) const
+            {
+                std::cout << freq_;
+                double temp = 0;
+                double sum = 0;
+                boost::circular_buffer<double> buffer(8);
+                for(unsigned int m = 0; m < 8; m++)
+                  buffer.push_front(0);
+
+                for(int n = 0; n < in_out_size; n++)
+                {
+                  sum = 0;
+                  for(unsigned int m = 0; m < buffer.size(); m++)
+                    sum += buffer[m] * -a_[m +1];
+
+                  temp = in[n] + sum;
+
+                  sum = temp * b_[0];
+                  for(unsigned int m = 0; m < buffer.size(); m++)
+                    sum += buffer[m] * b_[m +1];
+
+                  out[n] = sum;
+
+                  buffer.push_front(temp);
+                }
+            }
+
+    static const unsigned int FILTER_ORDER;
+    static const double POLE_DISTANCE;
+    std::vector<double> a_;
+    std::vector<double> b_;
+    double d_;
+
+    double fs_;
+    double freq_;
+};
+
+}
+
+//---------------------------------------------------------------------------------------
+
+#endif // NOTCH_FILTER8_H
