289e32cff2a9a4c1e3f89d513054621719c661d7
Clemens Brunner <clemens.brunner@tugraz.at>
2013-09-27 14:28:05 +0200

    Added GPL license header.

--- src/gui_impl/commands/adapt_channel_view_gui_command.h
+++ src/gui_impl/commands/adapt_channel_view_gui_command.h
@@ -1,3 +1,22 @@
+// Copyright 2013 Clemens Brunner, Thomas Brunner, Christoph Eibel,
+// Alois Schlögl, Oliver Terbu.
+
+// This file is part of SigViewer.
+//
+// SigViewer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SigViewer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SigViewer. If not, see <http://www.gnu.org/licenses/>.
+
+
 #ifndef VIEW_OPTIONS_GUI_COMMAND_H
 #define VIEW_OPTIONS_GUI_COMMAND_H
 
