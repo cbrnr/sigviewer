8d0a1dd88af9724e481342204445894764682cfc
Yida Lin <ital_fazioli@hotmail.com>
2016-12-14 00:11:59 -0500

    Channel info is now under each Stream

--- src/gui_impl/dialogs/basic_header_info_dialog.cpp
+++ src/gui_impl/dialogs/basic_header_info_dialog.cpp
@@ -150,35 +150,269 @@ void BasicHeaderInfoDialog::buildTree()
     tmp_item = new QTreeWidgetItem(root_item);
     // tmp_item ->setTextAlignment(1, Qt::AlignRight);
     tmp_item->setText(0, tr("Number"));
-    tmp_item->setText(1, QString::number(basic_header_->getNumberEvents()));
+    tmp_item->setText(1, QString::number(basic_header_->getNumberEvents()).append(tr(" events")));
     tmp_item = new QTreeWidgetItem(root_item);
     // tmp_item ->setTextAlignment(1, Qt::AlignRight);
     tmp_item->setText(0, tr("Sample Rate"));
     tmp_item->setText(1, QString::number(basic_header_->getEventSamplerate()).append(tr(" Hz")));
 
     //exclusively for XDF
-
-    for (size_t i =0; i < XDFdata.streams.size(); i++)
+    if (basic_header_->getFileTypeString().indexOf("XDF") == 0)
     {
-        // basic
+        for (size_t i =0; i < XDFdata.streams.size(); i++)
+        {
+            // basic
+            root_item = new QTreeWidgetItem(info_tree_widget_);
+            root_item->setText(0, "Stream "+QString::number(i));
+            root_item->setIcon(0, QIcon(":/images/info_16x16.png"));
+            info_tree_widget_->setItemExpanded(root_item, true);
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("name"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.name));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("type"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.type));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("channel_count"));
+            if (XDFdata.streams[i].info.channel_count <= 1)
+            {
+                tmp_item->setText(1, QString::number(XDFdata.streams[i].info.channel_count).append(tr(" channel")));
+            }
+            else
+            {
+                tmp_item->setText(1, QString::number(XDFdata.streams[i].info.channel_count).append(tr(" channels")));
+            }
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("nominal_srate"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.nominal_srate).append(tr(" Hz")));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("channel_format"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.channel_format));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("source_id"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.source_id));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("version"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.version, 'f', 1));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("created_at"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.created_at));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("uid"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.uid));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("session_id"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.session_id));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("hostname"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.hostname));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("v4address"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.v4address));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("v4data_port"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.v4data_port));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("v4service_port"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.v4service_port));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("v6address"));
+            tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.v6address));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("v6data_port"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.v6data_port));
+
+            tmp_item = new QTreeWidgetItem(root_item);
+            tmp_item->setText(0, tr("v6service_port"));
+            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.v6service_port));
+
+            if (!XDFdata.streams[i].time_series.empty())
+            {
+                // channels
+                QTreeWidgetItem* XDFchannel;
+                XDFchannel = new QTreeWidgetItem(root_item);
+                XDFchannel->setText(0, tr("Channels"));
+                XDFchannel->setIcon(0, QIcon(":/images/channels_22x22.png"));
+                info_tree_widget_->setItemExpanded(XDFchannel, true);
+
+                for (uint32 channel_nr = 0;
+                     channel_nr < XDFdata.streams[i].info.channel_count;
+                     channel_nr++)
+                {
+                    QTreeWidgetItem* channel_item;
+                    int streamMapIndex;
+                    for (size_t str = 0; str < XDFdata.streamMap.size(); str++)
+                    {
+                        if (XDFdata.streamMap[str].first == i)
+                        {
+                            streamMapIndex = str;
+                            break;
+                        }
+                    }
+                    int channelIndex = XDFdata.streamMap[streamMapIndex].second - XDFdata.streams[i].info.channel_count + channel_nr;
+                    QSharedPointer<SignalChannel const> channel = basic_header_->getChannel (channelIndex);
+                    channel_item = new QTreeWidgetItem(XDFchannel);
+                    channel_item->setText(0, QString("(%1) %2").arg(channelIndex)
+                                                        .arg(channel->getLabel()));
+
+                    // channel basic
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Label"));
+                    tmp_item->setText(1, channel->getLabel());
+                    tmp_item = new QTreeWidgetItem(channel_item);
+
+                    tmp_item->setText(0, tr("Sample Rate"));
+                    float64 fs = channel->getSampleRate();
+                    if (fs < 0.0)
+                        fs = basic_header_->getSampleRate();
+                    tmp_item->setText(1, QString::number(fs).append(tr(" Hz")));
+
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Physical Dimension"));
+                    tmp_item->setText(1, channel->getPhysicalDim());
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Physical Maximum"));
+                    tmp_item->setText(1, QString::number(channel->getPhysicalMaximum()));
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Physical Minimum"));
+                    tmp_item->setText(1, QString::number(channel->getPhysicalMinimum()));
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Digital Maximum"));
+                    tmp_item->setText(1, QString::number(channel->getDigitalMaximum()));
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Digital Minimum"));
+                    tmp_item->setText(1, QString::number(channel->getDigitalMinimum()));
+                    tmp_item = new QTreeWidgetItem(channel_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Data Type"));
+                    tmp_item->setText(1, channel->typeString());
+
+                    // filter
+                    QTreeWidgetItem* filter_item;
+                    filter_item = new QTreeWidgetItem(channel_item);
+                    filter_item->setText(0, tr("Filter"));
+                    tmp_item = new QTreeWidgetItem(filter_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Highpass"));
+                    //tmp_item->setText(1, isnan(channel->getHighpass()) ? tr("unknown") :
+                    //    (channel->getHighpass() < 0 ? "" :
+                    //                       QString::number(channel->getHighpass())));
+                    tmp_item->setText(2, tr("Hz"));
+                    tmp_item = new QTreeWidgetItem(filter_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Lowpass"));
+                   // tmp_item->setText(1, channel->getLowpass() ? tr("unknown") :
+                    //                (channel->getLowpass() < 0 ? "" :
+                     //                           QString::number(channel->getLowpass())));
+                    tmp_item->setText(2, tr("Hz"));
+                    tmp_item = new QTreeWidgetItem(filter_item);
+                    // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+                    tmp_item->setText(0, tr("Notch"));
+                    //tmp_item->setText(1, isnan(channel->getNotch()) ? tr("unknown") : (channel->getNotch() ? tr("yes") : tr("no")));
+                }
+            }
+        }
+    }   //XDF ends here
+    else
+    {
+        // channels
         root_item = new QTreeWidgetItem(info_tree_widget_);
-        root_item->setText(0, "Stream "+QString::number(i));
-        root_item->setIcon(0, QIcon(":/images/info_16x16.png"));
+        root_item->setText(0, tr("Channels"));
+        root_item->setIcon(0, QIcon(":/images/channels_22x22.png"));
         info_tree_widget_->setItemExpanded(root_item, true);
 
-        tmp_item = new QTreeWidgetItem(root_item);
-        tmp_item->setText(0, tr("name"));
-        tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.name));
-
-        tmp_item = new QTreeWidgetItem(root_item);
-        tmp_item->setText(0, tr("type"));
-        tmp_item->setText(1, QString::fromStdString(XDFdata.streams[i].info.type));
-
-        tmp_item = new QTreeWidgetItem(root_item);
-        tmp_item->setText(0, tr("channel_count"));
-        if (XDFdata.streams[i].info.channel_count <= 1)
+        for (uint32 channel_nr = 0;
+             channel_nr < basic_header_->getNumberChannels();
+             channel_nr++)
         {
-            tmp_item->setText(1, QString::number(XDFdata.streams[i].info.channel_count).append(tr(" channel")));
+            QTreeWidgetItem* channel_item;
+            QSharedPointer<SignalChannel const> channel = basic_header_->getChannel (channel_nr);
+            channel_item = new QTreeWidgetItem(root_item);
+            channel_item->setText(0, QString("(%1) %2").arg(channel_nr + 1)
+                                                .arg(channel->getLabel()));
+
+            // channel basic
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Label"));
+            tmp_item->setText(1, channel->getLabel());
+            tmp_item = new QTreeWidgetItem(channel_item);
+
+            tmp_item->setText(0, tr("Sample Rate"));
+            float64 fs = channel->getSampleRate();
+            if (fs < 0.0)
+                fs = basic_header_->getSampleRate();
+            tmp_item->setText(1, QString::number(fs).append(tr(" Hz")));
+
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Physical Dimension"));
+            tmp_item->setText(1, channel->getPhysicalDim());
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Physical Maximum"));
+            tmp_item->setText(1, QString::number(channel->getPhysicalMaximum()));
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Physical Minimum"));
+            tmp_item->setText(1, QString::number(channel->getPhysicalMinimum()));
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Digital Maximum"));
+            tmp_item->setText(1, QString::number(channel->getDigitalMaximum()));
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Digital Minimum"));
+            tmp_item->setText(1, QString::number(channel->getDigitalMinimum()));
+            tmp_item = new QTreeWidgetItem(channel_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Data Type"));
+            tmp_item->setText(1, channel->typeString());
+
+            // filter
+            QTreeWidgetItem* filter_item;
+            filter_item = new QTreeWidgetItem(channel_item);
+            filter_item->setText(0, tr("Filter"));
+            tmp_item = new QTreeWidgetItem(filter_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Highpass"));
+            //tmp_item->setText(1, isnan(channel->getHighpass()) ? tr("unknown") :
+            //    (channel->getHighpass() < 0 ? "" :
+            //                       QString::number(channel->getHighpass())));
+            tmp_item->setText(2, tr("Hz"));
+            tmp_item = new QTreeWidgetItem(filter_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Lowpass"));
+           // tmp_item->setText(1, channel->getLowpass() ? tr("unknown") :
+            //                (channel->getLowpass() < 0 ? "" :
+             //                           QString::number(channel->getLowpass())));
+            tmp_item->setText(2, tr("Hz"));
+            tmp_item = new QTreeWidgetItem(filter_item);
+            // tmp_item ->setTextAlignment(1, Qt::AlignRight);
+            tmp_item->setText(0, tr("Notch"));
+            //tmp_item->setText(1, isnan(channel->getNotch()) ? tr("unknown") : (channel->getNotch() ? tr("yes") : tr("no")));
         }
         else
         {
