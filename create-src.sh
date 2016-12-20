e471bc270577e6f248276f93b05961949dd06422
Clemens Brunner <clemens.brunner@gmail.com>
2016-12-20 15:33:35 +0100

    Update create source script

--- create-src.sh
+++ create-src.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-version=`head -n 1 src/version.txt`
+version_major=`sed -n -e 's/^VERSION_MAJOR = \([0-9]*\)$/\1/p' sigviewer.pro`
+version_minor=`sed -n -e 's/^VERSION_MINOR = \([0-9]*\)$/\1/p' sigviewer.pro`
+version_build=`sed -n -e 's/^VERSION_BUILD = \([0-9]*\)$/\1/p' sigviewer.pro`
+version=$version_major.$version_minor.$version_build
 
 mkdir -p sigviewer-$version-src
 cp * sigviewer-$version-src
 cp -r deb_building_stuff sigviewer-$version-src
-mkdir -p sigviewer-$version-src/external
 cp -r src sigviewer-$version-src/src
 
-tar czvf sigviewer-$version-src.tar.gz --exclude=.* --exclude=*.pro.user --exclude=Makefile* sigviewer-$version-src
+rm sigviewer-$version-src/*.pro.user
+find sigviewer-$version-src -name "Makefile*" -delete
 
+tar czvf sigviewer-$version-src.tar.gz --exclude=.* sigviewer-$version-src
 rm -rf sigviewer-$version-src
