971889ded4b8f83eb3a40576752a32a30393dccc
Clemens Brunner <clemens.brunner@gmail.com>
2016-12-20 10:00:11 +0100

    Finalize parser

--- src/main.cpp
+++ src/main.cpp
@@ -33,59 +33,23 @@ int main(int argc, char* argv[])
 
     QCommandLineParser parser;
     parser.setApplicationDescription("SigViewer - a biosignal viewer.");
-    parser.addPositionalArgument("file", QApplication::translate("main", "File to open (optional)."));
+    parser.addPositionalArgument("file", QApplication::translate("main", "Input file (optional)."));
     parser.addHelpOption();
     parser.addVersionOption();
-    parser.addOptions({{{"t", "test"}, QApplication::translate("main", "Run tests.")}});
     parser.process(app);
 
-//    try
-//    {
-//        QMap<QString, QString> parameter_map = readCommandlineParameters(application.arguments());
-//        QString input_file_name;
-        std::set<ApplicationMode> app_modes;
-//        if (parameter_map.count("help"))
-//            return 1;
-//        if (parameter_map.contains("input-file"))
-//            input_file_name = parameter_map["input-file"];
-//        if (parameter_map.contains("test"))
-//            app_modes.insert(APPLICATION_TEST_MODE);
-//        if (parameter_map.contains("convert-to-gdf"))
-//            app_modes.insert(APPLICATION_NON_GUI_MODE);
-//        if (parameter_map.contains("event-codes"))
-//        {
-//            QSettings settings;
-//            if (parameter_map["event-codes"].size())
-//                settings.setValue("eventcodes_file", parameter_map["event-codes"]);
-//            else
-//                settings.remove("eventcodes_file");
-//        }
+    const QStringList args = parser.positionalArguments();
 
     GuiActionFactory::getInstance()->initAllCommands();
+    std::set<ApplicationMode> app_modes;
     ApplicationContextImpl::init(app_modes);
 
-//        if (app_modes.count(APPLICATION_TEST_MODE))
-//            GuiActionFactory::getInstance()->getQAction("Running tests...")->trigger();
-//        else if (parameter_map.contains("convert-to-gdf"))
-//        {
-//            std::cout << "Converting to GDF is in testing phase. Meta-data will not be converted." << std::endl;
-//            ConvertFileCommand convert_command(ApplicationContextImpl::getInstance(), input_file_name, parameter_map["output-file"]);
-//            QString error = convert_command.execute();
-//            if (error.size())
-//                std::cout << error.toStdString() << std::endl;
-//            return 0;
-//        }
-//        else if (input_file_name.size())
-//            OpenFileGuiCommand::openFile(input_file_name);
+    if (!args.isEmpty())
+        OpenFileGuiCommand::openFile(args[0]);
 
     int result = app.exec();
 
     ApplicationContextImpl::cleanup();
+
     return result;
-//    }
-//    catch (std::exception& e)
-//    {
-//        std::cerr << "Exception " << e.what() << std::endl;
-//    }
-    return 0;
 }
