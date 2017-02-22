fd99853a9ef04a62487cae5cfecac761939fa54d
Yida Lin <ital_fazioli@hotmail.com>
2017-02-22 17:46:29 -0500

    BugFix

--- src/gui_impl/signal_browser/y_axis_widget_4.cpp
+++ src/gui_impl/signal_browser/y_axis_widget_4.cpp
@@ -152,7 +152,7 @@ void YAxisWidget::paintYAxisLabels (QPainter* painter, float64 offset,
     }
 
     double value = 0;
-    for (float64 value_y = offset + height / 2; value_y < offset + height;
+    for (float64 value_y = offset + height / 2; value_y < offset + 10 * height;
          value_y += y_grid_pixel_intervall)
     {
         painter->drawLine (width () - 5, value_y, width () - 1, value_y);
@@ -164,7 +164,7 @@ void YAxisWidget::paintYAxisLabels (QPainter* painter, float64 offset,
 
     value = 0;
     for (float64 value_y = offset + height / 2 - y_grid_pixel_intervall;
-         value_y > offset; value_y -= y_grid_pixel_intervall)
+         value_y > offset - 10 * height; value_y -= y_grid_pixel_intervall)
     {
         value += value_range_fragment;
         painter->drawLine (width () - 5, value_y, width () - 1, value_y);
