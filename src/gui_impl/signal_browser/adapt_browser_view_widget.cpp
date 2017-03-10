decfdf301598cb0174fd63468d4444796659b27b
Yida Lin <yida.lin@outlook.com>
2017-03-10 14:38:07 -0500

    Fix channel per page box error

--- src/gui_impl/signal_browser/adapt_browser_view_widget.cpp
+++ src/gui_impl/signal_browser/adapt_browser_view_widget.cpp
@@ -154,8 +154,9 @@ void AdaptBrowserViewWidget::updateValues ()
     ui_.labels_checkbox_->setChecked (signal_visualisation_view_->getLabelsVisibility ());
     ui_.yGridSlider->setValue (settings_->getGridFragmentation(Qt::Vertical));
     //ui_.xGridSlider->setValue (settings_->getGridFragmentation(Qt::Horizontal));
-    ui_.channelsPerPageSpinbox->setValue (signal_visualisation_view_->getViewportHeight() /
-                                          settings_->getChannelHeight());
+    ui_.channelsPerPageSpinbox->setValue (ceil(signal_visualisation_view_->getViewportHeight() /
+                                          (double)settings_->getChannelHeight()));
+
     ui_.secsPerPageSpinbox->setValue ((signal_visualisation_view_->getViewportWidth() /
                                        settings_->getPixelsPerSample()) /
                                       settings_->getChannelManager().getSampleRate());
