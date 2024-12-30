80eb5f4553f801ff6b261834cdebdd579ee70fb9
Yida Lin <yl3842@columbia.edu>
2024-12-29 22:57:17 -0800

    uncomment

--- src/gui_impl/signal_browser/event_creation_widget.cpp
+++ src/gui_impl/signal_browser/event_creation_widget.cpp
@@ -11,6 +11,31 @@
 
 namespace sigviewer
 {
+namespace
+{
+
+[[nodiscard]] int uniqueXdfEventCount() {
+    std::unordered_set<std::string> events;
+    for (const auto& [stream_id, stream] : XDFdata->streams)
+    {
+        if (std::holds_alternative<std::vector<std::vector<std::string>>>(
+                    stream.time_series))
+        {
+            const auto& time_series = std::get<
+                    std::vector<std::vector<std::string>>>(stream.time_series);
+            for (const std::vector<std::string>& channel : time_series)
+            {
+                for (const std::string& sample : channel)
+                {
+                    events.insert(sample);
+                }
+            }
+        }
+    }
+    return events.size();
+}
+
+} // namespace
 
 //-----------------------------------------------------------------------------
 EventCreationWidget::EventCreationWidget (QSharedPointer<SignalVisualisationModel> signal_visualisation_model,
@@ -25,7 +50,7 @@ EventCreationWidget::EventCreationWidget (QSharedPointer<SignalVisualisationMode
     if (event_manager_->getFileType().startsWith("XDF", Qt::CaseInsensitive))
     {
         ui_.lineEdit->setPlaceholderText("Customize Event Text");
-//        customized_event_id_ = XDFdata->dictionary.size();
+        customized_event_id_ = uniqueXdfEventCount();
         ui_.groupBox->setToolTip("Select or customize an event type then click anywhere on the signals to create new events");
     }
     else //custom event seems doesn't work in files other than XDF
@@ -59,9 +84,10 @@ int EventCreationWidget::insertNewEventType()
 
         ui_.type_combobox_->setCurrentIndex(customized_event_id_);
         customized_event_id_++;
-//        if (customized_event_id_ >= 254) //Sigviewer has only 255 slots for custom events
-//            customized_event_id_ = XDFdata->dictionary.size();
-
+        if (customized_event_id_ >= 254) //Sigviewer has only 255 slots for custom events
+        {
+            customized_event_id_ = uniqueXdfEventCount();
+        }
         emit newEventType(event_manager_->getEventTypes());
     }
 
