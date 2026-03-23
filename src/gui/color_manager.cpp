dee669f9cac79772e9ed6f6fe5f931c21e5ec713
Clemens Brunner <clemens.brunner@gmail.com>
2026-03-23 11:13:18 +0100

    Build on Qt < 6.5

--- src/gui/color_manager.cpp
+++ src/gui/color_manager.cpp
@@ -197,7 +197,11 @@ void ColorManager::loadDefaultEventColors ()
 {
     QFile color_settings_file (":/color_settings.xml");
     QDomDocument color_doc;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
     color_doc.setContent(&color_settings_file, QDomDocument::ParseOption::UseNamespaceProcessing);
+#else
+    color_doc.setContent(&color_settings_file, true);
+#endif
     QDomElement root = color_doc.documentElement();
 
     QDomNodeList default_elements = root.elementsByTagName("default");
